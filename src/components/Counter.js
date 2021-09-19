ff89829272b1729089149583e5edba0db574aa02
jbmakib <mdfoujolmobin@gmail.com>
2021-09-19 17:23:19 +0600

    counter updated

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Counter() {
+    const [counter, setCounter] = useState(0);
+
+    const handleClick = (num) => {
+        if (counter + num < 0) {
+            return;
+        }
+        setCounter(counter + num);
+    };
+
     return (
         <div className="mb-3 text-center">
-            <h3>Counter: </h3>
-            <button className="btn btn-primary me-1">Increase</button>
-            <button className="btn btn-outline-primary ms-1">Decrease</button>
+            <h3>Counter: {counter}</h3>
+            <button
+                className="btn btn-primary me-1"
+                onClick={() => handleClick(1)}
+            >
+                Increase
+            </button>
+            <button
+                className="btn btn-outline-primary ms-1"
+                onClick={() => handleClick(-1)}
+            >
+                Decrease
+            </button>
         </div>
     );
 }
