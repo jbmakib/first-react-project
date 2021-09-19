a70c38e7171789d8480b4388812b30937a45d205
jbmakib <mdfoujolmobin@gmail.com>
2021-09-19 16:27:12 +0600

    Added counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Posts from "./components/Posts";
 import React from "react";
+import Posts from "./components/Posts";
+import Counter from "./components/Counter";
 
 function App() {
     return (
         <div className="container my-3">
+            <Counter></Counter>
             <Posts></Posts>
         </div>
     );
---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Counter() {
+    return (
+        <div className="mb-3 text-center">
+            <h3>Counter: </h3>
+            <button className="btn btn-primary me-1">Increase</button>
+            <button className="btn btn-outline-primary ms-1">Decrease</button>
+        </div>
+    );
+}
